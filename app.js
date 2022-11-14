d45d833a85875d4fd7b805b5f01ee9a6570dffd7
Ron Gabay <88055204+RonGabay@users.noreply.github.com>
2022-11-14 14:35:36 +0200

    Add files via upload
    Working with the Document Object Model
    Construction of a model in which I refreshed all the different concepts in DOM

---
+++ app.js
@@ -0,0 +1,42 @@
+// const h1 = document.getElementById('main-title');
+// h1.textContent = 'Some new title';
+// h1.style.color = 'white';
+// h1.style.backgroundColor = 'black';
+
+// const li = document.querySelector('li:last-of-type');
+// li.textContent = li.textContent + ' (Changed)';
+
+// const body = document.body;
+
+// // const listItemElements = document.querySelectorAll('li');
+// const listItemElements = document.getElementsByTagName('li'); // live list = reflect changes
+
+// for (const listItemEl of listItemElements) {
+//   console.dir(listItemEl);
+// }
+
+
+// DOM TRIVERSAL //
+const ul = document.body.firstElementChild.nextElementSibling;
+const firstLi = ul.firstElementChild;
+
+console.log(firstLi);
+
+// CSS manipulation //
+const section = document.querySelector('section');
+const button = document.querySelector('button');
+
+// section.style.backgroundColor = 'blue';
+section.className = 'red-bg';
+
+button.addEventListener('click', () => {
+  // if(section.className === 'red-bg visible') {
+  //   section.className = 'red-bg invisible';
+  // } else {
+  //   section.className = 'red-bg visible';
+  // }
+
+  // Same as above (less code)
+  // section.classList.toggle('visible');
+  section.classList.toggle('invisible');
+});
